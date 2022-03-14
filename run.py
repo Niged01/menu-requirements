f236d39dbf96587c25939c21011afa2bd0ca8680
Niged01 <nigedoberer@gmail.com>
2022-03-14 11:48:43 +0000

    Created get_passenger_numbers_data function to get number of flight and passengers from user

--- run.py
+++ run.py
@@ -12,8 +12,16 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('menu_requirements')
 
-passengers = SHEET.worksheet('passengers')
+def get_passenger_numbers_data():
+    """
+    Get passenger numbers from User
+    """
+    print("Please enter flight number and number of passengers")
+    print("Enter flight number bis class passenger and crew numbers, seperated by commas.")
+    print("Example: LI300,50,140,10\n")
 
-data = passengers.get_all_values()
+    input_str = input("Enter your data here: ")
+    print(f"The data provided is {input_str}")
 
-print(data)
+
+get_passenger_numbers_data()
