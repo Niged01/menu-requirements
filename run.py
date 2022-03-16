b4a89b6c61726a491e4c7d4896999e4af3bc09d2
Niged01 <nigedoberer@gmail.com>
2022-03-16 13:50:07 +0000

    Created bis class function for business class meals

--- run.py
+++ run.py
@@ -43,32 +43,35 @@ def validated_passenger_data(passenger_numbers):
             raise ValueError(
                 f"Exactly 4 values required, you provided {len(passenger_numbers)}"
             )
-    
 
-        if passenger_numbers[0] in range(0, 1000): 
+        if passenger_numbers[0] in range(0, 1000):
             pass
-        else: raise ValueError(
+        else: 
+            raise ValueError(
                 f"A number between 0-999 required, you provided {passenger_numbers[0]}"
                 )
             
 
         if passenger_numbers[1] in range(0, 61): 
             pass
-        else: raise ValueError(
+        else: 
+            raise ValueError(
                 f"A number between 0-60 required, you provided {passenger_numbers[1]}"
                 )
         
 
         if passenger_numbers[2] in range(0, 251): 
             pass
-        else: raise ValueError(
+        else: 
+            raise ValueError(
                 f"A number between 0-250 required, you provided {passenger_numbers[2]}"
                 )
         
 
         if passenger_numbers[3] in range(0, 14): 
             pass
-        else: raise ValueError(
+        else: 
+            raise ValueError(
                 f"A number between 0-13 required, you provided {passenger_numbers[3]}"
                 )
     except ValueError as e:
@@ -85,21 +88,56 @@ def update_passenger_worksheet(data):
     """
     print("Updating Passenger numbers...\n")
     passenger_worksheet = SHEET.worksheet("passengers")
+    # Prefix flight number with `LI`.
+    data[0] = f"LI{data[0]}"
+    # flight_number, biz, economy, crew = data
+    # flight_number = f"LI{flight_number}"
     passenger_worksheet.append_row(data)
     print("Passenger numbers updated sucessfully.\n")
 
-def economy_crew_meals():
+def economy_crew_meals(passenger_data):
     """
     collects flight number, adds economy passengers and crew together 
-    adds 20 divides total by two.
+    adds 20 divides total by two. 
     """
-    flight_number = passenger_data[0]
-    meals = round(passenger_data[2] + passenger_data[3] + 20 / 2)
-    print(flight_number, meals)
+    return round((passenger_data[2] + passenger_data[3] + 20) / 2)
 
+    
 
+def update_economycrew_worksheet(passenger_data, economy_meals):
+    """
+    Updates economycrew meal numbers in work sheet adding new row
+    """
+    print("Updating meal numbers...\n")
+    data = [passenger_data[0], economy_meals, economy_meals]
+    economycrew_worksheet = SHEET.worksheet("economycrew")
+    economycrew_worksheet.append_row(data)
+    print("Passenger meals updated sucessfully.\n")
+
+def bis_class_meals(passenger_data):
+    """
+    collects flight number, adds economy passengers and crew together 
+    adds 20 divides total by two. 
+    """
+    return round(passenger_data[1] / 6)
+
+def update_bisclass_worksheet(passenger_data, economy_meals):
+    """
+    Updates economycrew meal numbers in work sheet adding new row
+    """
+    print("Updating meal numbers...\n")
+    data = [passenger_data[0], bisclass_meals, bisclass_meals, bisclass_meals, bisclass_meals, bisclass_meals, bisclass_meals]
+    economycrew_worksheet = SHEET.worksheet("bis")
+    economycrew_worksheet.append_row(data)
+    print("Passenger meals updated sucessfully.\n")
 
 data = get_passenger_numbers_data()
 passenger_data = [int(num) for num in data]
 update_passenger_worksheet(passenger_data)
-economy_crew_meals()
+economy_meals = economy_crew_meals(passenger_data)
+update_economycrew_worksheet(passenger_data, economy_meals)
+bisclass_meals = bis_class_meals(passenger_data)
+update_bisclass_worksheet(passenger_data, bisclass_meals)
+
+
+
