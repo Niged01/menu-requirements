b97a2afd0adf6eb8f8c93cc8fabf48b06716e87e
Niged01 <nigedoberer@gmail.com>
2022-03-16 08:44:19 +0000

    Refactored Valitated_passenger_date function to run correctly

--- run.py
+++ run.py
@@ -32,45 +32,48 @@ def get_passenger_numbers_data():
     return passenger_numbers
 
 
-def validated_passenger_data(values):
+def validated_passenger_data(passenger_numbers):
+    """
+    Validates the data input from user, gives error message if incorrect.
+    """
 
     try:
-        [int(value) for value in values]
-        if len(values) != 4:
+        passenger_numbers = [int(value) for value in passenger_numbers]
+        if len(passenger_numbers) != 4:
             raise ValueError(
-                f"Exactly 4 values required, you provided {len(values)}"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-        return False
-
-
-    if passenger_numbers[0] in range(0, 999): 
-        pass
-    else: raise ValueError(
-            f"A number between 0-999 required, you provided {passenger_numbers[0]}"
+                f"Exactly 4 values required, you provided {len(passenger_numbers)}"
             )
     
 
-    if passenger_numbers[1] in range(0, 60): 
-        pass
-    else: raise ValueError(
-            f"A number between 0-60 required, you provided {passenger_numbers[1]}"
-            )
-     
+        if passenger_numbers[0] in range(0, 1000): 
+            pass
+        else: raise ValueError(
+                f"A number between 0-999 required, you provided {passenger_numbers[0]}"
+                )
+            
 
-    if passenger_numbers[2] in range(0, 250): 
-        pass
-    else: raise ValueError(
-            f"A number between 0-250 required, you provided {passenger_numbers[2]}"
-            )
-     
+        if passenger_numbers[1] in range(0, 61): 
+            pass
+        else: raise ValueError(
+                f"A number between 0-60 required, you provided {passenger_numbers[1]}"
+                )
+        
 
-    if passenger_numbers[3] in range(0, 13): 
-        pass
-    else: raise ValueError(
-            f"A number between 0-13 required, you provided {passenger_numbers[3]}"
-            )
+        if passenger_numbers[2] in range(0, 251): 
+            pass
+        else: raise ValueError(
+                f"A number between 0-250 required, you provided {passenger_numbers[2]}"
+                )
+        
+
+        if passenger_numbers[3] in range(0, 14): 
+            pass
+        else: raise ValueError(
+                f"A number between 0-13 required, you provided {passenger_numbers[3]}"
+                )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
      
 
     return True
